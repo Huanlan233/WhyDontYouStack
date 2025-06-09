658fa47c5e38db0879891472a9b7fed4008b0dc5
Huanlan233 <huanlan233@outlook.com>
2025-06-09 20:59:46 +0800

    feat: Init

--- src/main/java/top/htext/whydontyoustack/mixin/ShulkerBoxesMixin.java
+++ src/main/java/top/htext/whydontyoustack/mixin/ShulkerBoxesMixin.java
@@ -2,24 +2,14 @@ package top.htext.whydontyoustack.mixin;
 
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShulkerBoxBlock;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.context.LootContextParameterSet;
-import net.minecraft.server.MinecraftServer;
-import org.apache.commons.logging.Log;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-import top.htext.whydontyoustack.WhyDontYouStack;
 
-import javax.swing.*;
 import java.util.List;
-
-import static top.htext.whydontyoustack.WhyDontYouStack.*;
 
 @Mixin(ShulkerBoxBlock.class)
 public class ShulkerBoxesMixin {
